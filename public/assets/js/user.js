6f9d7e7e48b79f9f8083cb423e407ba1340b34b6
pengyufang <525524802@qq.com>
2020-03-15 14:03:59 +0800

    表单提交

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -6,20 +6,20 @@ let userArr = [];
 $.ajax({
     type: 'get',
     url: '/users',
-    success: function(response) {
+    success: function (response) {
         userArr = response;
         render();
     }
 })
 function render() {
-    let html = template('usertpl',{userdata: userArr});
+    let html = template('usertpl', { userdata: userArr });
     $('tbody').html(html);
 }
 //完成用户添加的时候写入
 //ajax实现用户添加功能
-$('#avatar').on('change',function() {
+$('#avatar').on('change', function () {
     let formData = new FormData();
-    formData.append('avatar',this.files[0]);
+    formData.append('avatar', this.files[0]);
     $.ajax({
         type: 'post',
         url: '/upload',
@@ -27,10 +27,39 @@ $('#avatar').on('change',function() {
         //通过jq的ajax上传文件就要设置下面两个属性
         processData: false,
         contentType: false,
-        success: function(res) {
-            $('#preImg').attr('src',res[0].avatar);
+        success: function (res) {
+            $('#preImg').attr('src', res[0].avatar);
             //还要将图片的地址保存在表单的控件属性中
             $('#hidden').val(res[0].avatar)
+        }
+    })
+})
+
+//完成用户添加功能
+$("#btnAdd").on('click', function () {
+    //获取所有表单控件的内容
+    let data = $('form').serialize();
+    $.ajax({
+        type: 'post',
+        url: '/users',
+        data: data,
+        success: function (res) {
+            userArr.push(res);
+            render();
+
+            //将表单数据清空
+            $('input[type="email"]').val('');
+            $('input[name="nickName"]').val('');
+            $('input[name="password"]').val('');
+            $('#status0').prop('checked', false);
+            $('#status1').prop('checked', false);
+            $('#admin').prop('checked', false);
+            $('#normal').prop('checked', false);
+            $('#hidden').val('');
+            $('#preImg').attr('src', '../assets/img/default.png')
+        },
+        error: function() {
+            console.log(err)
         }
     })
 })
