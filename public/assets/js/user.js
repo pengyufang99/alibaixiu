f39720d5a172e5273f5fa26d363024704c1aeeb8
pengyufang <525524802@qq.com>
2020-03-15 13:51:04 +0800

    pengyufang图片上传

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -15,3 +15,22 @@ function render() {
     let html = template('usertpl',{userdata: userArr});
     $('tbody').html(html);
 }
+//完成用户添加的时候写入
+//ajax实现用户添加功能
+$('#avatar').on('change',function() {
+    let formData = new FormData();
+    formData.append('avatar',this.files[0]);
+    $.ajax({
+        type: 'post',
+        url: '/upload',
+        data: formData,
+        //通过jq的ajax上传文件就要设置下面两个属性
+        processData: false,
+        contentType: false,
+        success: function(res) {
+            $('#preImg').attr('src',res[0].avatar);
+            //还要将图片的地址保存在表单的控件属性中
+            $('#hidden').val(res[0].avatar)
+        }
+    })
+})
