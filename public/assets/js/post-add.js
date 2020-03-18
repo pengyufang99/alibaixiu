dabedd858f1da53f7d586d3e721d4ac4ec5b7374
pengyufang <525524802@qq.com>
2020-03-18 15:44:45 +0800

    完成文章编辑功能

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -2,21 +2,21 @@
 $.ajax({
     type: 'get',
     url: '/categories',
-    success: function(res) {
+    success: function (res) {
         // console.log(res)
-        let html = template('cateTpl',{data:res});
+        let html = template('cateTpl', { data: res });
         $("#category").html(html);
     }
 });
 
 //图片上传
-$('#feature').on('change',function() {
+$('#feature').on('change', function () {
     //获取文件
     var file = this.files[0];
     //创建formData
     var formData = new FormData();
     //将文件追加到formData对象中
-    formData.append('cover',file);
+    formData.append('cover', file);
     //实现文章封面图片上传
     $.ajax({
         type: 'post',
@@ -24,23 +24,89 @@ $('#feature').on('change',function() {
         data: formData,
         processData: false,
         contentType: false,
-        success: function(res) {
+        success: function (res) {
             //图片预览
-            $('.thumbnail').show().attr('src',res[0].cover)
+            $('.thumbnail').show().attr('src', res[0].cover)
             $('#hidden').val(res[0].cover)
         }
     })
 });
 
 //完成文章添加功能
-$('#btnAdd').on('click',function() {
+$('#btnAdd').on('click', function () {
     let data = $('form').serialize();
     $.ajax({
         type: 'post',
         url: '/posts',
         data: data,
-        success: function(res) {
+        success: function (res) {
             location.href = '/admin/posts.html';
+        }
+    })
+})
+
+
+//封装函数，判断id值，是修改，还是添加
+function getParams(key) {
+    let params = location.search.substr(1).split('&');
+    for (var i = 0; i < params.length; i++) {
+        //key=value,将字符串转为数组
+        let temp = params[i].split('=');
+        if (temp[0] === key) {
+            return temp[1]
+        }
+    }
+    return -1;
+}
+
+//调用函数
+let id = getParams('id');
+
+if (id != -1) {
+    $.ajax({
+        url: '/posts/' + id,
+        type: 'get',
+        success: function (res) {
+            console.log(res);
+
+            $('h1').text('编辑文章');
+            $('#title').val(res.title);
+            $("#content").val(res.content);
+            $(".thumbnail").show().attr('src', res.thumbnail);
+            $("#created").val(res.createAt.substr(0, 16));
+
+            $("#category option").each(function (index,item) {
+                console.log($(item).attr('value'));
+                if ($(item).attr('value') == res.category._id) {
+                    $(item).prop('selected', true)
+                }
+            });
+
+
+            $("#status option").each(function (index,item) {
+                if ($(item).attr('value') == res.state) {
+                    $(item).prop('selected', true)
+                }
+            });
+
+            //隐藏保存
+            $('#btnAdd').hide();
+            //显示修改
+            $("#btnEdit").show();
+        }
+    })
+}
+
+
+//保存修改
+$('#btnEdit').on('click',function() {
+    let data = $('form').serialize();
+    $.ajax({
+        type: 'put',
+        url: '/posts/'+id,
+        data: data,
+        success: function() {
+            location.href = 'posts.html';
         }
     })
 })
--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,12 +1,50 @@
+//获取分类和状态
+var cid = $('#category').val();
+var state = $('#state').val();
 //发送ajax 请求文章列表
+render(cid, state)
+
+
+
+function changePage(index) {
+    render(cid, state, index)
+}
+
+//获取所有的文章分类
 $.ajax({
     type: 'get',
-    url: '/posts',
-    data: {
-        page: 1
-    },
-    success: function(res) {
-        let html = template('pTpl',{data:res.records});
-        $('tbody').html(html);
+    url: '/categories',
+    success: function (res) {
+        let html = template('cTpl', { data: res });
+        $('#category').append(html);
     }
+});
+
+//筛选功能
+$('#search').on('click', function () {
+    cid = $('#category').val();
+    state = $('#state').val();
+    //发送ajax
+    render(cid, state);
 })
+
+//封装render函数
+function render(cid, state, page = 1) {
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: {
+            page: page,
+            category: cid,
+            state: state
+        },
+        success: function (res) {
+            let html = template('pTpl', { data: res.records });
+            $('tbody').html(html);
+
+            let pageHtml = template('pageTpl', res);
+            // console.log(pageHtml)
+            $('.pagination').html(pageHtml);
+        }
+    })
+}
